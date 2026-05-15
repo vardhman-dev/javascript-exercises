7ed7b9fba07719c06fe71ed075bd19198df874de
Vardhman Jain <229144856+vardhman-dev@users.noreply.github.com>
2026-05-15 07:36:32 +0000

    Exercise done

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    if(str===""){
+        return "";
+    }
 
+    return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
