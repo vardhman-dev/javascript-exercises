8ba8d285dff2e651200767409a32b0723e05881c
Vardhman Jain <229144856+vardhman-dev@users.noreply.github.com>
2026-05-15 07:22:18 +0000

    Exercise done

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if(num < 0){
+        return "ERROR";
+    }
+    
+    if(num===0 || str === ""){
+        return "";
+    }
+    initial = str;
+    for(let i = 1; i<num; i++){
+        str = str + initial;
+    }
+    return str;
 };
 
 // Do not edit below this line
