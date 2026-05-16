e8decd539140fb274858170dafaed83c2e59b5be
Vardhman Jain <229144856+vardhman-dev@users.noreply.github.com>
2026-05-16 02:33:28 +0000

    Exercise done

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    if (Number.isInteger(num1) && Number.isInteger(num2) && num1>=0 && num2>=0) {
 
+        if (Number.isFinite(num1) && Number.isFinite(num2)) {
+
+            if (num2 > num1) {
+                sum = 0
+                for (let i = num1; i <= num2; i++) {
+                    sum += i;
+                }
+                return sum;
+            }
+            else {
+                sum = 0
+                for (let i = num2; i <= num1; i++) {
+                    sum += i;
+                }
+                return sum;
+            }
+            
+        }
+        return "ERROR";
+    }
+    return "ERROR";
 };
 
 // Do not edit below this line
